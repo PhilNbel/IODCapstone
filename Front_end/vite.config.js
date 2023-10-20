e9711d11b74620fd8ebbd595d36a85d37e8191b1
Philippe Hennebelle <nopespower@gmail.com>
2023-10-20 16:04:04 +1300

    changed port from api to front end

--- Front_end/vite.config.js
+++ Front_end/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port:80,
     proxy: {
       '/api': 'http://localhost:3000/',
       '/img': 'http://localhost:3000/'
