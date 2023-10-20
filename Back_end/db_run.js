db999a12400be5a4f404215649d3c3a3cb48d1a8
Philippe Hennebelle <nopespower@gmail.com>
2023-10-20 18:33:44 +1300

    Checking the user to know which .env is used

--- Back_end/db_run.js
+++ Back_end/db_run.js
@@ -1,6 +1,8 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
+console.log(DB_HOST)
+console.log(DUB_USER)
 process.env.DB_HOST=(process.env.DB_HOST!='localhost')?process.env.DB_HOST:"prod-db.c5n9s8zzabuw.ap-southeast-2.rds.amazonaws.com";
 
 // Create a connection to the database
